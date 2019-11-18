8d84e322a32908721c528e12dce1caae379facac
Charley2018 <charleschan2016@m.scnu.edu.cn>
2019-11-18 19:52:27 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Charley2018 <charleschan2016@m.scnu.edu.cn>

---
+++ src/main/java/cn/lm/dao/UsersDao.java
@@ -0,0 +1,19 @@
+/**
+ * Filename: UsersDao
+ * Date: 2019/11/18 19:51
+ * Author: Charles Chan
+ * Description:
+ * History:
+ * Version: 1.0
+ */
+
+package cn.lm.dao;
+
+/**
+ * @author Charles Chan
+ * @create 2019/11/18
+ * @since 1.0
+ */
+
+public class UsersDao {
+}
